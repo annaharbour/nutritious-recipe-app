98fca41de9a80ae7639eaaab8a85301714406d1f
Arrowpierced <101281738+Arrowpierced@users.noreply.github.com>
2023-09-11 17:17:48 -0400

    removed dependencies from useeffect array in recipeform to stop infinite looping

--- client/src/Components/recipe-forms/RecipeForm.js
+++ client/src/Components/recipe-forms/RecipeForm.js
@@ -36,7 +36,7 @@ const RecipeForm = ({
     };
 
     fetchAllIngredients();
-  }, [fetchIngredientCategories, fetchIngredientsByCategory, categories]);
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
