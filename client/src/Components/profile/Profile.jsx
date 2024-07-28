e27c217aafb5ee5c9788234fae290a7a38c8c81e
Anna Harbour <101281738+annaharbour@users.noreply.github.com>
2024-07-28 10:54:20 -0400

    fixed profile pw update for oauth users

--- client/src/Components/profile/Profile.jsx
+++ client/src/Components/profile/Profile.jsx
@@ -1,184 +1,376 @@
+// import React, { useState, useEffect } from "react";
+// import { getUser, updateUser, deleteUser } from "../../services/userService";
+
+// function Profile({ showToast }) {
+// 	const [loading, setLoading] = useState(false);
+// 	const [userInfo, setUserInfo] = useState({});
+// 	const [changePw, setChangePw] = useState(false);
+// 	const [formData, setFormData] = useState({
+// 		name: "",
+// 		email: "",
+// 		phone: "",
+// 		password: "",
+// 		newPassword: "",
+// 		newPassword2: "",
+// 	});
+// 	const { name, email, phone, password, newPassword, newPassword2 } = formData;
+
+// 	useEffect(() => {
+// 		const fetchProfileInfo = async () => {
+// 			try {
+// 				setLoading(true);
+// 				let response = await getUser();
+// 				console.log(response);
+// 				setUserInfo(response);
+// 				const { name, email, phone } = response;
+// 				setFormData({
+// 					name: name || "",
+// 					email: email || "",
+// 					phone: phone || "",
+// 					password: "",
+// 					newPassword: "",
+// 					newPassword2: "",
+// 				});
+// 				setLoading(false);
+// 			} catch (error) {
+// 				setLoading(false);
+// 			}
+// 		};
+
+// 		fetchProfileInfo();
+// 	}, []);
+
+// 	const deleteAccount = async () => {
+// 		if (window.confirm("Are you sure you want to delete your account?")) {
+// 			try {
+// 				const res = await deleteUser(userInfo._id);
+// 				console.log(res.data);
+// 			} catch (error) {
+// 				console.error(error);
+// 			}
+// 		}
+// 	};
+
+// 	const onChange = (e) =>
+// 		setFormData({ ...formData, [e.target.name]: e.target.value });
+
+// 	const onSubmit = async (e) => {
+// 		e.preventDefault();
+// 		if (changePw) {
+// 			if (newPassword !== newPassword2) {
+// 				showToast("Passwords do not match", "error");
+// 				return;
+// 			}
+// 		}
+// 		try {
+// 			await updateUser(
+// 				name,
+// 				email,
+// 				phone,
+// 				password,
+// 				changePw ? newPassword : null
+// 			);
+
+// 			showToast("Profile updated successfully", "success");
+// 		} catch (error) {
+// 			showToast("Update failed. Please try again.", "error");
+// 		}
+// 	};
+
+// 	return (
+// 		<div className="account">
+// 			<section className="container">
+// 				<h3 className="large text-primary">Change Account Details</h3>
+// 				<h3>{userInfo.name}</h3>
+// 				<h4>
+// 					<i className="fas fa-user" /> Update Your Information
+// 				</h4>
+// 				<form className="form" onSubmit={onSubmit}>
+// 					<div className="form-group">
+// 						<input
+// 							type="text"
+// 							placeholder="Name"
+// 							name="name"
+// 							value={name || ""}
+// 							onChange={onChange}
+// 							required
+// 						/>
+// 					</div>
+// 					<div className="form-group">
+// 						<input
+// 							type="email"
+// 							placeholder="Email Address"
+// 							name="email"
+// 							value={email || ""}
+// 							onChange={onChange}
+// 							required
+// 						/>
+// 					</div>
+// 					<div className="form-group">
+// 						<input
+// 							type="text"
+// 							placeholder="Phone Number"
+// 							name="phone"
+// 							value={phone || ""}
+// 							onChange={onChange}
+// 							required
+// 						/>
+// 					</div>
+
+// 					<div className="form-group">
+// 						{userInfo.oAuthProvider && !userInfo.password ? null : (
+// 							<input
+// 								required
+// 								type="password"
+// 								placeholder="Password"
+// 								name="password"
+// 								value={password || ""}
+// 								onChange={onChange}
+// 							/>
+// 						)}
+// 						<i onClick={() => setChangePw(!changePw)}>
+// 							{!changePw ? (
+// 								<i className="fa-solid fa-lock"></i>
+// 							) : (
+// 								<i className="fa-solid fa-lock-open"></i>
+// 							)}{" "}
+// 							Update password?{" "}
+// 						</i>
+// 						{changePw && (
+// 							<div>
+// 								<h4>
+// 									<i className="fa-solid fa-key"></i>Change your password
+// 								</h4>
+// 								<div className="form-group">
+// 									<input
+// 										type="password"
+// 										placeholder="New Password"
+// 										name="newPassword"
+// 										value={newPassword || ""}
+// 										onChange={onChange}
+// 										required
+// 									/>
+// 								</div>
+// 								<div className="form-group">
+// 									<input
+// 										type="password"
+// 										placeholder="Confirm Password"
+// 										name="newPassword2"
+// 										value={newPassword2 || ""}
+// 										onChange={onChange}
+// 										required
+// 									/>
+// 								</div>
+// 							</div>
+// 						)}
+// 					</div>
+
+// 					<button
+// 						type="submit"
+// 						disabled={loading}
+// 						className="btn btn-primary"
+// 						value="Update">
+// 						Submit
+// 					</button>
+
+// 					<button
+// 						className="btn btn-warning"
+// 						disabled={loading}
+// 						onClick={deleteAccount}>
+// 						Delete Account
+// 					</button>
+// 				</form>
+// 			</section>
+// 		</div>
+// 	);
+// }
+
+// export default Profile;
 import React, { useState, useEffect } from "react";
 import { getUser, updateUser, deleteUser } from "../../services/userService";
 
 function Profile({ showToast }) {
-	const [loading, setLoading] = useState(false);
-	const [userInfo, setUserInfo] = useState({});
-	const [changePw, setChangePw] = useState(false);
-	const [formData, setFormData] = useState({
-		name: "",
-		email: "",
-		phone: "",
-		password: "",
-		newPassword: "",
-		newPassword2: "",
-	});
-	const { name, email, phone, password, newPassword, newPassword2 } = formData;
+    const [loading, setLoading] = useState(false);
+    const [userInfo, setUserInfo] = useState({});
+    const [changePw, setChangePw] = useState(false);
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        password: "",
+        newPassword: "",
+        newPassword2: "",
+    });
+    const { name, email, phone, password, newPassword, newPassword2 } = formData;
 
-	useEffect(() => {
-		const fetchProfileInfo = async () => {
-			try {
-				setLoading(true);
-				let response = await getUser();
-				console.log(response)
-				setUserInfo(response);
-				const { name, email, phone } = response;
-				setFormData({
-					name: name || "",
-					email: email || "",
-					phone: phone || "",
-					password: "",
-					newPassword: "",
-					newPassword2: "",
-				});
-				setLoading(false);
-			} catch (error) {
-				setLoading(false);
-			}
-		};
+    useEffect(() => {
+        const fetchProfileInfo = async () => {
+            try {
+                setLoading(true);
+                let response = await getUser();
+                console.log(response);
+                setUserInfo(response);
+                const { name, email, phone } = response;
+                setFormData({
+                    name: name || "",
+                    email: email || "",
+                    phone: phone || "",
+                    password: "",
+                    newPassword: "",
+                    newPassword2: "",
+                });
+                setLoading(false);
+            } catch (error) {
+                setLoading(false);
+            }
+        };
 
-		fetchProfileInfo();
-	}, []);
+        fetchProfileInfo();
+    }, []);
 
-	const deleteAccount = async () => {
-		if (window.confirm("Are you sure you want to delete your account?")) {
-			try {
-				const res = await deleteUser(userInfo._id);
-				console.log(res.data);
-			} catch (error) {
-				console.error(error);
-			}
-		}
-	};
+    const deleteAccount = async () => {
+        if (window.confirm("Are you sure you want to delete your account?")) {
+            try {
+                const res = await deleteUser(userInfo._id);
+                console.log(res.data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    };
 
-	const onChange = (e) =>
-		setFormData({ ...formData, [e.target.name]: e.target.value });
+    const onChange = (e) =>
+        setFormData({ ...formData, [e.target.name]: e.target.value });
 
-	const onSubmit = async (e) => {
-		e.preventDefault();
-		if (changePw) {
-			if (newPassword !== newPassword2) {
-				showToast("Passwords do not match", "error");
-				return;
-			}
-		}
-		try {
-			await updateUser(
-				name,
-				email,
-				phone,
-				password,
-				changePw ? newPassword : null
-			);
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        if (changePw) {
+            if (newPassword !== newPassword2) {
+                showToast("Passwords do not match", "error");
+                return;
+            }
+        }
+        try {
+            await updateUser(
+                name,
+                email,
+                phone,
+                password || null,
+                changePw ? newPassword : null
+            );
 
-			showToast("Profile updated successfully", "success");
-		} catch (error) {
-			showToast("Update failed. Please try again.", "error");
-		}
-	};
+            showToast("Profile updated successfully", "success");
+        } catch (error) {
+            showToast("Update failed. Please try again.", "error");
+        }
+    };
 
-	return (
-		<div className="account">
-			<section className="container">
-				<h4 className="large text-primary">Change Account Details</h4>
-				<h3>{userInfo.name}</h3>
-				<p className="lead">
-					<i className="fas fa-user" /> Update Your Information
-				</p>
-				<form className="form" onSubmit={onSubmit}>
-					<div className="form-group">
-						<input
-							type="text"
-							placeholder="Name"
-							name="name"
-							value={name || ""}
-							onChange={onChange}
-							required
-						/>
-					</div>
-					<div className="form-group">
-						<input
-							type="email"
-							placeholder="Email Address"
-							name="email"
-							value={email || ""}
-							onChange={onChange}
-							required
-						/>
-					</div>
-					<div className="form-group">
-						<input
-							type="text"
-							placeholder="Phone Number"
-							name="phone"
-							value={phone || ""}
-							onChange={onChange}
-							required
-						/>
-					</div>
-					<div className="form-group">
-						<input
-							required
-							type="password"
-							placeholder="Password"
-							name="password"
-							value={password || ""}
-							onChange={onChange}
-						/>
-						<i onClick={() => setChangePw(!changePw)}>
-							{!changePw ? (
-								<i className="fa-solid fa-lock"></i>
-							) : (
-								<i className="fa-solid fa-lock-open"></i>
-							)}{" "}
-							Update password?{" "}
-						</i>
-						{changePw && (
-							<div>
-								<h4>
-									<i className="fa-solid fa-key"></i>Change your password
-								</h4>
-								<div className="form-group">
-									<input
-										type="password"
-										placeholder="New Password"
-										name="newPassword"
-										value={newPassword || ""}
-										onChange={onChange}
-										required
-									/>
-								</div>
-								<div className="form-group">
-									<input
-										type="password"
-										placeholder="Confirm Password"
-										name="newPassword2"
-										value={newPassword2 || ""}
-										onChange={onChange}
-										required
-									/>
-								</div>
-							</div>
-						)}
-					</div>
-					<button
-						type="submit"
-						disabled={loading}
-						className="btn btn-primary"
-						value="Update">
-						Submit
-					</button>
+    return (
+        <div className="account">
+            <section className="container">
+                <h3 className="large text-primary">Change Account Details</h3>
+                <h3>{userInfo.name}</h3>
+                <h4>
+                    <i className="fas fa-user" /> Update Your Information
+                </h4>
+                <form className="form" onSubmit={onSubmit}>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            placeholder="Name"
+                            name="name"
+                            value={name || ""}
+                            onChange={onChange}
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="email"
+                            placeholder="Email Address"
+                            name="email"
+                            value={email || ""}
+                            onChange={onChange}
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            placeholder="Phone Number"
+                            name="phone"
+                            value={phone || ""}
+                            onChange={onChange}
+                            required
+                        />
+                    </div>
 
-					<button
-						className="btn btn-warning"
-						disabled={loading}
-						onClick={deleteAccount}>
-						Delete Account
-					</button>
-				</form>
-			</section>
-		</div>
-	);
+                    <div className="form-group">
+                        {userInfo.oAuthProvider && !userInfo.hasPassword ? null : (
+                            <input
+                                required
+                                type="password"
+                                placeholder="Password"
+                                name="password"
+                                value={password || ""}
+                                onChange={onChange}
+                            />
+                        )}
+                        <i onClick={() => setChangePw(!changePw)}>
+                            {!changePw ? (
+                                <i className="fa-solid fa-lock"></i>
+                            ) : (
+                                <i className="fa-solid fa-lock-open"></i>
+                            )}{" "}
+                            Update password?{" "}
+                        </i>
+                        {changePw && (
+                            <div>
+                                <h4>
+                                    <i className="fa-solid fa-key"></i>Change your password
+                                </h4>
+                                <div className="form-group">
+                                    <input
+                                        type="password"
+                                        placeholder="New Password"
+                                        name="newPassword"
+                                        value={newPassword || ""}
+                                        onChange={onChange}
+                                        required
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        type="password"
+                                        placeholder="Confirm Password"
+                                        name="newPassword2"
+                                        value={newPassword2 || ""}
+                                        onChange={onChange}
+                                        required
+                                    />
+                                </div>
+                            </div>
+                        )}
+                    </div>
+
+                    <button
+                        type="submit"
+                        disabled={loading}
+                        className="btn btn-primary"
+                        value="Update">
+                        Submit
+                    </button>
+
+                    <button
+                        className="btn btn-warning"
+                        disabled={loading}
+                        onClick={deleteAccount}>
+                        Delete Account
+                    </button>
+                </form>
+            </section>
+        </div>
+    );
 }
 
 export default Profile;
